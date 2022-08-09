b272b619b5c3fbeee2ecfc881c95979588299888
Spanarchian <spanarchian@gmail.com>
2022-08-09 23:43:12 +0100

    REFACTORED: main ADDED: stoics

--- app/main.py
+++ app/main.py
@@ -6,3 +6,55 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"code":418, "message": "I am not the teapot your looking for!"}
+
+
+
+@app.get("/stoics")
+async def root():
+    return {
+
+@app.get("/")
+async def root():
+    return {[
+        ["Waste no more time arguing what a good man should be. Be One.", " Marcus Aurelius"],
+
+["You could leave life right now. Let that determine what you do and say and think.", "Marcus Aurelius"],
+
+["He who fears death will never do anything worth of a man who is alive.", "Seneca"],
+
+["Life is very short and anxious for those who forget the past, neglect the present, and fear the future.", "Seneca"],
+
+["How long are you going to wait before you demand the best for yourself?.", "Epictetus"],
+
+["Don’t explain your philosophy. Embody it.", "Epictetus"],
+
+["You have power over your mind .", " not outside events. Realize this, and you will find strength.―Marcus Aurelius"],
+
+["Hang on to your youthful enthusiasms .", " you’ll be able to use them better when you’re older.―Seneca"],
+
+["Wealth consists not in having great possessions, but in having few wants.―Epictetus"],
+
+["If it is not right, do not do it; if it is not true, do not say it.", " Marcus Aurelius"],
+
+
+["Begin at once to live, and count each separate day as a separate life.", "Seneca"],
+
+["Stop drifting…Sprint to the finish. Write off your hopes, and if your well-being matters to you, be your own savior while you can.", "Marcus Aurelius"],
+
+["Whatever can happen at any time can happen today.", "Seneca"],
+
+["They lose the day in expectation of the night, and the night in fear of the dawn.", "Seneca"],
+
+["Let us prepare our minds as if we’d come to the very end of life. Let us postpone nothing. Let us balance life’s books each day… The one who puts the finishing touches on their life each day is never short of time.", "Marcus Aurelius"],
+
+["True happiness is to enjoy the present, without anxious dependence upon the future, not to amuse ourselves with either hopes or fears but to rest satisfied with what we have, which is sufficient, for he that is so wants nothing. The greatest blessings of mankind are within us and within our reach. A wise man is content with his lot, whatever it may be, without wishing for what he has not.―Seneca"],
+
+["The key is to keep company only with people who uplift you, whose presence calls forth your best.―Epictetus"],
+
+["The happiness of your life depends upon the quality of your thoughts.―Marcus Aurelius"],
+
+["If you want to improve, be content to be thought foolish and stupid.―Epictetus"],
+
+["Luck is what happens when preparation meets opportunity.","Seneca"]
+]
+    }
